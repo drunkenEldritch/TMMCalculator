e4fe819c14398bef8890327bf83f9cb5f09bf5bf
dreld <drunkenEldritch@gmail.com>
2018-02-18 03:18:28 +0100

    Fix layout of DialogFragment and add ScrollView

--- app/src/main/java/com/dreldritch/tmmcalculator/controller/PositionDialogFragment.kt
+++ app/src/main/java/com/dreldritch/tmmcalculator/controller/PositionDialogFragment.kt
@@ -2,14 +2,19 @@ package com.dreldritch.tmmcalculator.controller
 
 
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v4.app.Fragment
+import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.dreldritch.tmmcalculator.R
+import kotlinx.android.synthetic.main.fragment_position_dialog.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 /**
@@ -21,9 +26,22 @@ class PositionDialogFragment : DialogFragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater!!.inflate(R.layout.fragment_position_dialog, container, false)
     }
+
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val timeformatter = SimpleDateFormat("hh:mm")
+        val dateformatter = SimpleDateFormat("yyyy/MM/dd")
+        val time = timeformatter.format(Date())
+        val date = dateformatter.format(Date())
+
+        pos_date_edit.setText(date)
+        pos_time_edit.setText(time)
+    }
+
 
     companion object {
         // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,6 +54,7 @@ class PositionDialogFragment : DialogFragment() {
             /*args.putString(ARG_PARAM1, param1)
             args.putString(ARG_PARAM2, param2)
             fragment.arguments = args*/
+
             return fragment
         }
     }
