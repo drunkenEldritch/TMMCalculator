e58ac417273a06b05f19fc386675798f5ee7bec5
dreld <drunkenEldritch@gmail.com>
2018-02-23 22:42:38 +0100

    Use Android Room model instead of direct SQL implementation

--- app/src/main/java/com/dreldritch/tmmcalculator/controller/PositionDialogFragment.kt
+++ app/src/main/java/com/dreldritch/tmmcalculator/controller/PositionDialogFragment.kt
@@ -1,24 +1,23 @@
 package com.dreldritch.tmmcalculator.controller
 
-
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.dreldritch.tmmcalculator.R
-import com.dreldritch.tmmcalculator.model.Position
-import com.dreldritch.tmmcalculator.model.sqlitedb.PositionDBProcessor
+import com.dreldritch.tmmcalculator.model.roomdb.ItemData
+import com.dreldritch.tmmcalculator.model.roomdb.ItemRepository
 import kotlinx.android.synthetic.main.fragment_position_dialog.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 //TODO Test if fragment is dimissed when clicked on activity while fragment is open
-
+//TODO Check date format before adding into DB
 class PositionDialogFragment : DialogFragment() {
 
-    private var position: Position? = null
+    private var item: ItemData? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_position_dialog, container, false)
@@ -27,41 +26,48 @@ class PositionDialogFragment : DialogFragment() {
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val dbprocessor = PositionDBProcessor(context)
-        if(savedInstanceState != null)
-            position = savedInstanceState.getParcelable(POSITION) as Position
+        /*val itemViewModel = ViewModelProviders.of(this.activity).get(ItemViewModel::class.java)*/
+        val item_repo = ItemRepository(activity.application)
 
-        if(position != null){
-            setPositionFields(position!!)
+        //Edit-event. Fragment is used to edit an existing item object
+        /*if(savedInstanceState != null)
+            item = savedInstanceState.getParcelable(ITEM) as ItemData
+
+        if(item != null){
+            setItemFields(item!!)
             setDateField(Date())
-        }else
+        }else*/
             setDateField(Date())
 
         pos_abort_btn.setOnClickListener { dismiss() }
 
         pos_ok_button.setOnClickListener {
-            if(position == null){
-                position = Position(null,
+            if(item == null){
+                item = ItemData(null,
                         pos_name_edit.text.toString(),
                         pos_description_edit.text.toString(),
                         pos_price_edit.text.toString().toDouble(),
+                        pos_currency_spinner.getSelectedItem().toString(),
                         pos_date_edit.text.toString())
             }
-            dbprocessor.savePosition(null, position!!)
+
+            //Add item to DB
+            //TODO add DB insert(async)
+            item_repo.insert(item!!)
             dismiss()
         }
     }
 
     companion object {
         // the fragment initialization parameters
-        private val POSITION = "position"
+        private val ITEM = "item"
 
-        fun newInstance(position: Position?): PositionDialogFragment {
+        fun newInstance(item: ItemData?): PositionDialogFragment {
             val fragment = PositionDialogFragment()
 
-            if(position == null){
+            if(item == null){
                 val args = Bundle()
-                args.putParcelable(POSITION, position)
+                args.putParcelable(ITEM, item)
                 fragment.arguments = args
             }
             return fragment
@@ -84,10 +90,10 @@ class PositionDialogFragment : DialogFragment() {
     /**
      * Load data of passed position into fragment views.
      */
-    private fun setPositionFields(position: Position){
-        pos_name_edit.setText(position._name)
-        pos_description_edit.setText(position._description)
-        pos_price_edit.setText(position._price.toString())
-        pos_date_edit.setText(position._date)
+    private fun setItemFields(item: ItemData){
+        pos_name_edit.setText(item.name)
+        pos_description_edit.setText(item.description)
+        pos_price_edit.setText(item.price.toString())
+        pos_date_edit.setText(item.date)
     }
 }// Required empty public constructor
