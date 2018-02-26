ff97e6b12737784d93e05d0c423296669cc2eb68
dreld <drunkenEldritch@gmail.com>
2018-02-27 00:58:30 +0100

    Implement 'filtered by month' adapter for ExpandableListView
    All item data from a month will be sorted and grouped by days. Heading
    and child design are added in the resource folder.

--- app/src/main/java/com/dreldritch/tmmcalculator/controller/ExpandableDateAdapter.kt
+++ app/src/main/java/com/dreldritch/tmmcalculator/controller/ExpandableDateAdapter.kt
@@ -1,55 +1,90 @@
 package com.dreldritch.tmmcalculator.controller
 
+import android.arch.lifecycle.LiveData
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseExpandableListAdapter
-import android.widget.ExpandableListAdapter
-import android.widget.SimpleExpandableListAdapter
+import android.widget.TextView
+import com.dreldritch.tmmcalculator.R
 import com.dreldritch.tmmcalculator.model.roomdb.ItemData
 
 /**
  * Created by cerox on 25.02.2018.
  */
-class ExpandableDateAdapter(context: Context, itemlist: ArrayList<ItemData>): BaseExpandableListAdapter() {
+class ExpandableDateAdapter(val context: Context, val itemlist: List<ItemData>): BaseExpandableListAdapter() {
+
+    var dateset = mutableListOf<String>()
+    var dayItemMap = mutableMapOf<String, List<ItemData>>()
+
+    init {
+        itemlist.distinctBy { it.date }
+                .forEach { i -> dateset.add(i.date)}
+
+        dateset.sort()
+
+        for(x in dateset)
+            dayItemMap.put(x, itemlist.filter { it.date == x })
+    }
 
     override fun getGroup(groupPosition: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return dateset[groupPosition]
     }
 
     override fun getGroupView(groupPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var view = convertView
+        if (view == null) {
+            val inf = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            view = inf.inflate(R.layout.month_group_heading, null)
+        }
+
+        val dayheading = view!!.findViewById(R.id.day_info_heading) as TextView
+        dayheading.text = dateset[groupPosition]
+        return view
     }
 
     override fun getGroupId(groupPosition: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return groupPosition.toLong()
     }
 
     override fun getGroupCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return dateset.size
     }
 
     override fun getChild(groupPosition: Int, childPosition: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return dayItemMap[dateset[groupPosition]]!!.get(childPosition)
     }
 
     override fun getChildView(groupPosition: Int, childPosition: Int, isLastChild: Boolean, convertView: View?, parent: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        var view = convertView
+        val itemInfo = getChild(groupPosition, childPosition) as ItemData
+
+        if (view == null) {
+            val infalInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            view = infalInflater.inflate(R.layout.item_info_child, null)
+        }
+
+        val itemName = view!!.findViewById(R.id.item_info) as TextView
+        itemName.text = itemInfo.name
+
+        return view
     }
 
     override fun getChildId(groupPosition: Int, childPosition: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return childPosition.toLong()
     }
 
     override fun getChildrenCount(groupPosition: Int): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+         return dayItemMap[dateset[groupPosition]]!!.size
     }
 
     override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return true
     }
 
     override fun hasStableIds(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return true
     }
 }
